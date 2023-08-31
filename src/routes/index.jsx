8800c5da346eea2f6828a8d1311285e1052899f5
Douglas Mateus <douglaspmateus@gmail.com>
2023-08-30 22:37:58 -0300

    ajustado o app.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "../pages/home";
 import Login from "../pages/login";
 import Register from "../pages/register";
 
-const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> },
-]);
-
-export default Router;
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
