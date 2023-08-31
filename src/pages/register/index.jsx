8962ae7197f2cd524cf52c6f0a6a3b10313758f5
Douglas Mateus <douglaspmateus@gmail.com>
2023-08-30 22:25:27 -0300

    setup inicial

---
+++ src/pages/register/index.jsx
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <h1>Cadastre-se</h1>;
+};
+
+export default Register;
---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home";
+import Login from "../pages/login";
+import Register from "../pages/register";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+]);
+
+export default Router;
