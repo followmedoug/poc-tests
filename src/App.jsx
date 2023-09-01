ba4acd6ff9813eb1c1ee3f17f00e9c3f3f471754
Douglas Mateus <douglaspmateus@gmail.com>
2023-08-31 22:26:47 -0300

    configure routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import Router from "./routes";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+]);
 
 const App = () => {
-  return <Router />;
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
